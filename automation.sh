4f26411218245f11317c1b87f355115a5ec298cc
himanshurahangdal <80570470+himanshurahangdal@users.noreply.github.com>
2022-08-07 13:20:24 +0530

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#task 2
 
 s3_bucket="upgraad-himanshurahangdale"
 myname="himanshu"
@@ -42,4 +42,37 @@ tar -cvf /tmp/${myname}-httpd-logs-${timestamp}.tar /var/log/apache2/*.log
 aws s3 \
 cp /tmp/${myname}-httpd-logs-${timestamp}.tar \
 s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
+
+
+
+
+#task 3
+
+filecheck=$([ -e /var/www/html/inventory.html ] && echo "file exists" || echo "file absent")
+if [[ $filecheck == *"file absent"* ]]; then
+        echo "**Creating new inventory file"
+        echo -e "Log Type\tTime Created\tType\tSize" > /var/www/html/inventory.html
+        filesize=$(wc -c /var/www/html/inventory.html | awk '{print $1}')
+        echo -e "httpd-logs\t$timestamp\t"tar"\t$filesize" >> /var/www/html/inventory.html
+
+else
+        file_size=$(wc -c /var/www/html/inventory.html | awk '{print $1}')
+        echo -e "httpd-logs\t$timestamp\t"tar"\t$file_size" >> /var/www/html/inventory.html
+fi
+
+rm -rf /tmp/*.tar
+
+croncheck="/etc/cron.d/automation"
+file_cron=$([ -e $croncheck ] && echo "file exists" || echo "file absent")
+if [[ $file_cron == *"file absent"* ]]; then
+	echo "**Creating a new crontab file"
+        echo "SHELL=/bin/sh" > $croncheck
+        echo "PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin" >> $croncheck
+        echo "5 * * * * root /root/Automation_Project/automation.sh" >> $croncheck
+else
+        echo "Crontab file is present"
+fi
+
+exit
+
 
